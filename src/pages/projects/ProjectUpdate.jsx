7c350917dd4a504b28500db4ebd484688630a69a
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2026-03-02 19:50:43 +0300

    update project add and update pages

--- src/pages/projects/ProjectUpdate.jsx
+++ src/pages/projects/ProjectUpdate.jsx
@@ -1,6 +1,92 @@
+import { ErrorMessage, Field, Formik } from "formik";
+import { Button, Form, FormField, Label } from "semantic-ui-react";
 
 export default function ProjectUpdate() {
+  let initialValue = {
+    "id": 1,
+    "title": "",
+    "summary": "",
+    "description": "",
+    "features": "",
+    "projectDate": "",
+    "techStack": "",
+    "liveDemoUrl": "",
+    "sourceCodeUrl": "",
+    "coverImage": ""
+  }
+
   return (
-    <div>ProjectUpdate</div>
+    <div>
+      <p className="page-title">Proje Ekle</p>
+
+      <Formik initialValues={initialValue} onSubmit={(values, { resetForm }) => { }}>
+        <Form className="ui form">
+          <FormField>
+            <Field name="title" placeholder="Proje başlığı"></Field>
+            <ErrorMessage name="title" render={error =>
+              <Label pointing basic color="red" content={error}></Label>
+            }></ErrorMessage>
+          </FormField>
+
+          <FormField>
+            <Field name="summary" placeholder="Proje özeti"></Field>
+            <ErrorMessage name="summary" render={error =>
+              <Label pointing basic color="red" content={error}></Label>
+            }></ErrorMessage>
+          </FormField>
+
+          <FormField>
+            <Field name="description" placeholder="Açıklama"></Field>
+            <ErrorMessage name="description" render={error =>
+              <Label pointing basic color="red" content={error}></Label>
+            }></ErrorMessage>
+          </FormField>
+
+          <FormField>
+            <Field name="features" placeholder="Proje Özellikleri"></Field>
+            <ErrorMessage name="features" render={error =>
+              <Label pointing basic color="red" content={error}></Label>
+            }></ErrorMessage>
+          </FormField>
+
+          <FormField>
+            <Field name="projectDate" placeholder="Proje Tarihi"></Field>
+            <ErrorMessage name="projectDate" render={error =>
+              <Label pointing basic color="red" content={error}></Label>
+            }></ErrorMessage>
+          </FormField>
+
+          <FormField>
+            <Field name="techStack" placeholder="Tech Stack"></Field>
+            <ErrorMessage name="techStack" render={error =>
+              <Label pointing basic color="red" content={error}></Label>
+            }></ErrorMessage>
+          </FormField>
+
+          <FormField>
+            <Field name="liveDemoUrl" placeholder="Canlı Demo"></Field>
+            <ErrorMessage name="liveDemoUrl" render={error =>
+              <Label pointing basic color="red" content={error}></Label>
+            }></ErrorMessage>
+          </FormField>
+
+          <FormField>
+            <Field name="sourceCodeUrl" placeholder="Kaynak Kodu"></Field>
+            <ErrorMessage name="sourceCodeUrl" render={error =>
+              <Label pointing basic color="red" content={error}></Label>
+            }></ErrorMessage>
+          </FormField>
+
+          <FormField>
+            <Field name="coverImage" placeholder="Kapak Resmi"></Field>
+            <ErrorMessage name="coverImage" render={error =>
+              <Label pointing basic color="red" content={error}></Label>
+            }></ErrorMessage>
+          </FormField>
+
+          <Button color="green" type="submit">Ekle</Button>
+        </Form>
+      </Formik>
+    </div>
   )
 }
