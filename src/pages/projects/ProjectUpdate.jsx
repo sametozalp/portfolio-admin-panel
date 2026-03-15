f9bc9b914bb067eb1d0f3536e5bee3e651bff144
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2026-03-15 15:30:38 +0300

    update project update page

--- src/pages/projects/ProjectUpdate.jsx
+++ src/pages/projects/ProjectUpdate.jsx
@@ -1,26 +1,23 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Button, FormField, Label } from "semantic-ui-react";
+import { Button, Checkbox, FormField, Icon, Image, Label } from "semantic-ui-react";
 import ProjectService from "../../service/projectService";
 
 export default function ProjectUpdate() {
 
   const { id } = useParams()
   const [project, setProject] = useState({
-    showable: "",
-    title: "",
-    summary: "",
-    description: "",
-    features: "",
-    coverImage: "",
-    images: "",
-    projectDate: "",
-    techStack: "",
-    liveDemoUrl: "",
-    sourceCodeUrl: ""
+    "title": "",
+    "summary": "",
+    "description": "",
+    "features": "",
+    "projectDate": "",
+    "techStack": "",
+    "liveDemoUrl": "",
+    "sourceCodeUrl": ""
   });
-  const service = useMemo(()=> new ProjectService(), [])
+  const service = useMemo(() => new ProjectService(), [])
 
   useEffect(() => {
     service.getById(id).then(r => {
@@ -29,14 +26,14 @@ export default function ProjectUpdate() {
   }, [service, id]);
 
   function submit(values) {
-    service.update(project.id, values)
+    service.update(project.id, values);
   }
 
   return (
     <div>
       <p className="page-title">Projeyi Güncelle</p>
 
-      <Formik initialValues={project} onSubmit={(values) => { submit(values) }} enableReinitialize>
+      <Formik initialValues={project} onSubmit={(values, { resetForm }) => { submit(values) }} enableReinitialize>
         <Form className="ui form">
           <FormField>
             <Field name="title" placeholder="Proje başlığı"></Field>
@@ -53,14 +50,14 @@ export default function ProjectUpdate() {
           </FormField>
 
           <FormField>
-            <Field name="description" placeholder="Açıklama"></Field>
+            <Field as="textarea" name="description" placeholder="Açıklama"></Field>
             <ErrorMessage name="description" render={error =>
               <Label pointing basic color="red" content={error}></Label>
             }></ErrorMessage>
           </FormField>
 
           <FormField>
-            <Field name="features" placeholder="Proje Özellikleri"></Field>
+            <Field as="textarea" name="features" placeholder="Proje Özellikleri"></Field>
             <ErrorMessage name="features" render={error =>
               <Label pointing basic color="red" content={error}></Label>
             }></ErrorMessage>
@@ -93,15 +90,42 @@ export default function ProjectUpdate() {
               <Label pointing basic color="red" content={error}></Label>
             }></ErrorMessage>
           </FormField>
-
-          <FormField>
-            <Field name="coverImage" placeholder="Kapak Resmi"></Field>
-            <ErrorMessage name="coverImage" render={error =>
-              <Label pointing basic color="red" content={error}></Label>
-            }></ErrorMessage>
-          </FormField>
-
           <Button color="green" type="submit">Güncelle</Button>
+        </Form>
+      </Formik>
+
+      <Formik
+        enableReinitialize
+      >
+        <Form className="ui form">
+          {
+            project.images?.map((img, index) => (
+              <FormField key={index} className="update-image-row">
+                <Label>Sıra Numarası: {img.orderNumber}</Label>
+                <Button><Icon name="chevron down" /></Button>
+                <Button><Icon name="chevron up" /></Button>
+                <Image
+                  src={img.url || '/images/image.png'}
+                  size='medium'
+                  style={{ width: '150px', height: '100px', objectFit: 'cover' }}
+                />
+                <Checkbox label={{ children: 'Make cover image' }} checked={true}  /*onChange={(e, data) => setIsCover(data.checked)}*/ />
+                <Button content="Sil" icon="trash" labelPosition="left" size="mini" color="red" /*onClick={() => handleDeleteProject(e.id)}*/ />
+              </FormField>
+            ))
+          }
+
+          {/* <FormField style={{ marginTop: "20px" }}>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => {
+                if (e.currentTarget.files && e.currentTarget.files[0]) {
+                  //newPhoto.current = e.currentTarget.files[0];
+                }
+              }}
+            />
+          </FormField> */}
         </Form>
       </Formik>
     </div>
