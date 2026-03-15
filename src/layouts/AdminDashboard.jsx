43815565c02c88ca271f799f84d347c99876f9a2
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2026-03-15 23:18:11 +0300

    add link (admin panel title)

--- src/layouts/AdminDashboard.jsx
+++ src/layouts/AdminDashboard.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 import { Grid } from 'semantic-ui-react'
 import AdminRouter from '../router/adminRouter'
 import AdminMenu from './AdminMenu'
@@ -6,14 +7,15 @@ export default function AdminDashboard() {
     return (
         <Grid columns={2} divided className='admin-dashboard' stackable>
             <Grid.Row>
-                <Grid.Column width={3}>
-                    <p>Admin Panel</p>
+                <Grid.Column width={2} className="admin-menu-column">
+                    <NavLink to="/admin" className="admin-panel-title">
+                        <p>Admin Panel</p>
+                    </NavLink>
                     <AdminMenu />
                 </Grid.Column>
-                <Grid.Column width={13}>
-                    <div className='admin-content'>
-                        <AdminRouter />
-                    </div>
+
+                <Grid.Column width={14} className="admin-content-column">
+                    <AdminRouter />
                 </Grid.Column>
             </Grid.Row>
         </Grid>
